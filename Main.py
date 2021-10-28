5907b192e9fd56395c6603aba0abab4cd94b7f4a
Linh Nguyen <nightkingst@gmail.com>
2021-10-28 13:13:47 +0700

    QuanLySinhVien

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from QuanLySinhVien import QuanLySinhVien
 qlsv = QuanLySinhVien()
 while (True):
     print(" ")
-    print(" ............Nhập yêu cầu................")
+    print(" ..............Nhập yêu cầu..............")
     print("|    0. Thoát chương trình               |")
     print("|    1. Nhập thông tin sinh viên         |")
     print("|    2. Cập nhật thông tin sinh viên     |")
@@ -13,49 +13,49 @@ while (True):
     print("|    5. Hiển thị danh sách sinh viên     |")
     print(" ''''''''''''''''''''''''''''''''''''''''")
 
-    chon = int(input("Mời bạn chọn: "))
+    chon = int(input(" Mời bạn chọn: "))
     if (chon == 0):
         break
     if (chon == 1):
-        print("\n1. Nhập thông tin sinh viên.")
+        print("\n 1. Nhập thông tin sinh viên.")
         qlsv.nhapSinhVien()
-        print("\nThêm sinh viên thành công!")
+        print("\n Thêm sinh viên thành công!")
     elif (chon == 2):
         if (qlsv.soLuongSinhVien() > 0):
-            print("\n2. Cập nhật thông tin sinh viên. ")
-            print("\nNhập mã của sinh viên cần sửa: ")
+            print("\n 2. Cập nhật thông tin sinh viên. ")
+            print("\n Nhập mã của sinh viên cần sửa: ")
             id = input()
             qlsv.updateSinhVien(id)
         else:
-            print("\nDanh sách rỗng!")
+            print("\n Danh sách rỗng!")
     elif (chon == 3):
         if (qlsv.soLuongSinhVien() > 0):
-            print("\n3. Xóa sinh viên.")
-            print("\nNhập mã sinh viên cần xóa: ")
+            print("\n 3. Xóa sinh viên.")
+            print("\n Nhập mã sinh viên cần xóa: ")
             id = input()
             if (qlsv.deleteById(id)):
-                print("\nSinh viên có mã: ", id, " đã bị xóa.")
+                print("\n Sinh viên có mã: ", id, " đã bị xóa.")
             else:
-                print("\nKhông có sinh viên nào có mã:", id)
+                print("\n Không có sinh viên nào có mã:", id)
         else:
-            print("\nDanh sách rỗng!")
+            print("\n Danh sách rỗng!")
     elif (chon == 4):
         if (qlsv.soLuongSinhVien() > 0):
-            print("\n4. Tìm kiếm sinh viên.")
-            print("\nNhập tên sinh viên cần tìm: : ")
+            print("\n 4. Tìm kiếm sinh viên.")
+            print("\n Nhập tên sinh viên cần tìm: : ")
             name = input()
             searchResult = qlsv.findByName(name)
             qlsv.showSinhVien(searchResult)
         else:
-            print("\nDanh sách rỗng!")
+            print("\n Danh sách rỗng!")
 
     elif (chon == 5):
         if (qlsv.soLuongSinhVien() > 0):
-            print("\n5. Hiển thị danh sách sinh viên.")
+            print("\n 5. Hiển thị danh sách sinh viên.")
             qlsv.showSinhVien(qlsv.getListSinhVien())
             soluong = qlsv.soLuongSinhVien()
-            print("Hiện có {} sinh viên trong danh sách".format(soluong))
+            print(" Hiện có {} sinh viên trong danh sách".format(soluong))
         else:
-            print("\nDanh sách rỗng!")
+            print("\n Danh sách rỗng!")
     else:
-        print("\nHãy chọn đúng chức năng trong menu.")
+        print("\n Hãy chọn đúng chức năng trong menu.")
