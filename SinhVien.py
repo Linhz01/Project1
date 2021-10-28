80ae3c3dcf619fa5b68c35d6fbd905364e99f20a
Linh Nguyen <nightkingst@gmail.com>
2021-10-28 13:16:55 +0700

    QuanLyDuAn

---
+++ SinhVien.py
@@ -0,0 +1,10 @@
+class SinhVien:
+ 
+    def __init__(self, id, name, sex, age, nganh, diemTB):
+        self._id = id
+        self._name = name
+        self._sex = sex
+        self._age = age
+        self._nganh = nganh
+        self._diemTB = diemTB
+        self._hocLuc = ""
